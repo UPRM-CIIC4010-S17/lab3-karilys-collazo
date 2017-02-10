0195ca5369d7708d036ac7b1466fd363d65dec1f
Lys <Lys@ProBook>
2017-02-10 19:39:31 -0400

    Puerto Rican flag has been added to the window.

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -1,7 +1,9 @@
 	import java.awt.Color;
 	import java.awt.Graphics;
 	import java.awt.Insets;
-	import javax.swing.JPanel;
+import java.awt.Polygon;
+
+import javax.swing.JPanel;
 	 
 	public class MyPanelClass extends JPanel {
 	            /**
@@ -22,29 +24,71 @@
 	                        int height = y2 - y1;
 	 
 	                        //Paint the background
-	                        g.setColor(Color.GRAY);
-	                        g.fillRect(x1, y1, width+1, height+1);
-	            /*
-	                      //Draw a border
-	                        g.setColor(Color.YELLOW);
-	                        g.drawRect(x1, y1, width, height);
-	                        
-	                        //Draw a second border
 	                        g.setColor(Color.RED);
-	                        int z=10;
-	                        g.drawRect(x1+z, y1+z, width-2*z, height-2*z);
-				
-							// Draw diagonal lines
-	                        g.setColor(Color.WHITE);
-	                        g.drawLine(x1, y1, x2, y2);
+	                        g.fillRect(x1, y1, width+1, height+1);
+//	          
+//	                      //Draw a border
+//	                        g.setColor(Color.YELLOW);
+//	                        g.drawRect(x1, y1, width, height);
+//	                        
+//	                        //Draw a second border
+//	                        g.setColor(Color.RED);
+//	                        int z=10;
+//	                        g.drawRect(x1+z, y1+z, width-2*z, height-2*z);
+//				
+//							// Draw diagonal lines
+//	                        g.setColor(Color.WHITE);
+//	                        g.drawLine(x1, y1, x2, y2);
+//	                        
+//	                        g.setColor(Color.BLUE);
+//	                        g.drawLine(x2, y1, x1, y2);
+//				
+//	                        int centralizeoval=55/2;
+//	                        g.setColor(Color.LIGHT_GRAY);
+//	                        g.fillOval(x2/2-centralizeoval, y2/2-centralizeoval, 55, 55);
+//	                        
+//    Polygon p = new Polygon();
+//    p.addPoint(x1 + 5, y1 + 25);
+//    p.addPoint(x1 + 20, y1 + 10);
+//    p.addPoint(x1 + 35, y1 + 25);
+//    p.addPoint(x1 + 25, y1 + 25);
+//    p.addPoint(x1 + 25, y1 + 45);
+//    p.addPoint(x1 + 15, y1 + 45);
+//    p.addPoint(x1 + 15, y1 + 25);
+//    g.setColor(Color.YELLOW);
+//    g.fillPolygon(p);
 	                        
-	                        g.setColor(Color.BLUE);
-	                        g.drawLine(x2, y1, x1, y2);
-				
-				*/
-	                        int centralizeoval=55/2;
-	                        g.setColor(Color.LIGHT_GRAY);
-	                        g.fillOval(x2/2-centralizeoval, y2/2-centralizeoval, 55, 55);
-	                 
-				}
+	         
+//Draw white stripes          
+	int space= y2/5;
+	g.setColor(Color.WHITE);
+	g.fillRect(x1, space, width+1, space);
+	g.fillRect(x1, 3*space, width+1, space);
+	                        
+//Draw Blue Triangle    
+    Polygon triangle= new Polygon();
+    triangle.addPoint(x1, y1);
+    triangle.addPoint(x1+115, y2/2);
+    triangle.addPoint(x1, y2+1);
+    g.setColor(Color.BLUE);
+    g.fillPolygon(triangle);
+
+//Draw star
+    Polygon p2 = new Polygon();
+    p2.addPoint(x1 + 25, y1 + 73);
+    p2.addPoint(x1 + 41, y1 + 73);
+    p2.addPoint(x1 + 47, y1 + 58);
+    p2.addPoint(x1 + 53, y1 + 73);
+    p2.addPoint(x1 + 69, y1 + 73);
+    p2.addPoint(x1 + 56, y1 + 83);
+    p2.addPoint(x1 + 61, y1 + 98);
+    p2.addPoint(x1 + 47, y1 + 88);
+    p2.addPoint(x1 + 34, y1 + 98);
+    p2.addPoint(x1 + 38, y1 + 83);
+    g.setColor(Color.WHITE);
+    g.fillPolygon(p2);
+       }
 	}
+	
+	
+	
